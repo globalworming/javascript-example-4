7ea09c783559e46b6a7468fefd2153006e8de35d
globalworming <andreas.worm@headissue.com>
2022-08-24 16:08:16 +0200

    array exercise

--- test.js
+++ test.js
@@ -2,12 +2,21 @@ const RunTest =  {
     test1() {
         setTimeout(() => {
             try {
-                let pineapples = document.getElementsByClassName("pineapple").length
-                if (!pineapples) {
-                    document.getElementById("displayOnSuccess").hidden = false
-                } else {
-                    document.getElementsByClassName("error")[0].textContent = `test failed, expected no pineapples, found ${pineapples}`
+                let foods = document.getElementsByClassName("foods")[0]
+                let textContent = foods.textContent;
+                if (textContent.includes("undefined")) {
+                    document.getElementsByClassName("error")[0].textContent = `test failed, expected no 'undefined' food`
+                    return
                 }
+                if (!textContent.includes("apple")) {
+                    document.getElementsByClassName("error")[0].textContent = `test failed, expected at least one 'apple'`
+                    return
+                }
+                if (!textContent.includes("banana")) {
+                    document.getElementsByClassName("error")[0].textContent = `test failed, expected at least one 'banana' food`
+                    return
+                }
+                document.getElementById("displayOnSuccess").hidden = false
             } catch (e) {
                 document.getElementsByClassName("error")[0].textContent = e.message + "\n\n" + e.stack
                 throw e
